f85f68d1f4373fa700cb95d1381d6fdc144b78ff
Sandro Reis <consultorsandro@hotmail.com>
2025-05-27 09:13:09 -0300

    Class 186 Turbofish

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-#[derive(Debug)] // Class 185
+#[derive(Debug)] // Class 186
 
 struct DeliSandwich {}
 
 fn main() {
-    println!("{}", identity(5));
+    println!("{}", identity::<i32>(5)); // Class 186
+    println!("{}", identity::<f64>(13.94));
+    println!("{}", identity::<&str>("Hello, Rust!"));
+    println!("{}", identity::<String>(String::from("Hello, Rust!")));
+    println!("{}", identity::<bool>(true));
+    println!("{:?}", identity::<DeliSandwich>(DeliSandwich {}));
+}
+
+fn identity<T>(value: T) -> T {
+    value
+}
+/* Class 185
+   println!("{}", identity(5));
     println!("{}", identity(13.94));
     println!("{}", identity("Hello, Rust!"));
     println!("{}", identity(String::from("Hello, Rust!")));
     println!("{}", identity(true));
     println!("{:?}", identity(DeliSandwich {}));
-}
-
-
-
-fn identity<T>(value: T) -> T {
-    value
-}
+*/
